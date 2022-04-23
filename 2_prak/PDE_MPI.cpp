c85208b7de242bb31be201fb1fee270cee3c88a7
Ilya-Lopatin <lopatin.ia@phystech.edu>
2022-04-23 03:16:47 +0300

    add input improve exact sol

--- 2_prak/PDE_MPI.cpp
+++ 2_prak/PDE_MPI.cpp
@@ -1,39 +1,48 @@
 #include <iostream>
 #include <mpi.h>
 #include <string>
+#include <fstream>
 #include <vector>
 #include <unistd.h>
 #include <cmath>
 
-long double h_in = 0.02 ,
-            k = 1 , 
-            l = 1 ,
-            u0 = 1 ,
+//default
+long double h = 0.02 ,
+            k = 1.0 , 
+            l = 1.0 ,
+            u0 = 1.0 ,
             dt = 0.0002 ,
-            t_target = 0.1;
+            t_target = 0.1 ,
+            pi = 3.14159265358979323846 ; 
 
-long double pi = 3.14159265358979323846 ;            
+unsigned int get_limit_sum ( const long double &eps ) 
+{
+    long double temp = pow(u0, 1.0 / 6 ) / ( pow( k * t_target, 1.0 / 2 ) * pow (eps, 1.0 / 6 ) );
+    temp *=  (l / ( pi * pow( 3*pi , 1.0 / 6 ) ) );
+    temp = ( 1 + temp ) / 2;
+    unsigned int n = ceil( temp );
+    if ( n >= 5 )
+        return n;
+    else
+        return 5;    
+}
 
 long double get_exact_sol ( 
-    const long double x, const long double t, const long double eps ) 
+    const long double &x, const long double &t , const long double &eps ) 
 {
-    if (   x <= 1e-7 ||  l - x <= 1e-7   )
+    if (   x <= 1e-8 ||  l - x <= 1e-8   )
         return 0;
-    long double crit = pi * eps * x / ( 5 * l * u0 );
     long double sum = 0; 
-    long int m = 0; 
-    long double a_m = exp( - k * pi * pi *  t / ( l * l )  ) / ( 2 * m +1 );
-    sum += a_m * sin ( pi * x / l );
-    while ( a_m >= crit ) {
-        ++m;
-        a_m = exp( - k * pi * pi * (2*m+1)*(2*m+1) * t / ( l * l )  ) / ( 2 * m +1 );
+    unsigned int lim_sum =  get_limit_sum ( eps );
+    for ( unsigned int m = 0; m <= lim_sum ; ++m ) {
+        long double a_m = exp( - k * pow(pi, 2) * pow(2*m + 1 , 2) * t / ( l * l )  ) / ( 2 * m +1 );
         sum +=  a_m  * sin ( pi * x * ( 2 * m +1 ) / l );
     }  
     return sum * 4 * u0 / ( pi );
 }
 
 long double exch_mess ( 
-    const int k, const long double* left, const long double* right, const int ph ) 
+    const int &k, const long double* left, const long double* right, const int &ph ) 
 {
     MPI_Status status;
     long double* get_value = new long double[1];
@@ -48,7 +57,6 @@ long double exch_mess (
             MPI_Recv( &get_value[0] , 1 , MPI_LONG_DOUBLE , k-1 , 0 , MPI_COMM_WORLD, &status );
         else
             MPI_Send ( &right[0], 1, MPI_LONG_DOUBLE , k+1 , 0, MPI_COMM_WORLD );
-
 
     if ( k % 2 == 0 )
         if ( ph == 1 )
@@ -71,13 +79,31 @@ int main ( int argc, char* argv[] )
     MPI_Status status;   
     int myrank, size, error;
     long double beginT, endT;
-
-    //long int Nnode =  ceil( l / h_in )+1 ;
+    
     long int Nnode = 51 ;
-    long double h = l / (Nnode-1);
-    //dt = ( h*h ) / (2 * k); 
-    dt =0.0002;
-    long double kr = (dt * k) / ( h*h );
+
+    std::ifstream IN ("./IN.txt");
+    std::string skip_l ,
+                skip_k ,
+                skip_u ,
+                skip_T ,
+                skip_N ,
+                skip_dt ;
+    
+    if (IN.is_open())
+    {
+        IN >> l >> skip_l >> k >> skip_k >> u0 >> skip_u ;
+        IN >> t_target >> skip_T >> Nnode >> skip_N >> dt >> skip_dt ;
+    }
+    else 
+    {
+        std::cerr<<"Can not open IN.txt"<<'\n';
+        exit(0);
+    }
+    IN.close(); 
+
+    long double h = l / ( Nnode - 1 );
+    long double kr = (dt * k) / ( h * h );
     std::vector<long double> ans;
     
     MPI_Init (&argc, &argv);
@@ -106,7 +132,6 @@ int main ( int argc, char* argv[] )
 
     while ( t_curr < t_target  ) 
     {
-        //printf("start %d", myrank);
         long double l, r;
         
         if ( myrank % 2 != 0 )
@@ -121,30 +146,26 @@ int main ( int argc, char* argv[] )
         if ( (myrank % 2 != 0) && (  myrank + 1 < size )  )
             r = exch_mess( myrank, &U[0], &U[my_quant-1] , 2 );    
 
-        for ( int i =1 ; i < my_quant-1 ; i++ )
+        for ( int i = 1 ; i < my_quant - 1 ; i++ )
             U_n[i] = kr * ( U[i+1] + U[i-1] - 2 * U[i] ) ;
 
         if ( myrank != 0 )
             U_n[0] = kr * ( U[1] + l - 2 * U[0] ) ;
         else
-            U_n[0] = 0 ;    
+            U_n[0] = 0 ;   
+
         if ( myrank != size - 1 )    
             U_n[ my_quant-1 ] = kr * ( U[my_quant-2] + r - 2 * U[ my_quant-1 ]  ) ;
         else
             U_n[ my_quant-1 ] = 0;
-        for ( int i =0; i < my_quant; ++i)
+        
+        for ( int i = 0; i < my_quant; ++i)
             U[i] += U_n[i];
 
         t_curr += dt;
         MPI_Barrier( MPI_COMM_WORLD );
     } 
 
-    /*
-    if ( myrank == size )
-        for ( int i = 0; i < my_quant; i++ )
-            printf("%Lf ", U[i] ); 
-    */
-    //int n = 1;
     if ( myrank > 0  )
         MPI_Send( &U[0] , my_quant  , MPI_LONG_DOUBLE , 0 , 0 , MPI_COMM_WORLD );
 
@@ -162,24 +183,18 @@ int main ( int argc, char* argv[] )
             delete [] mes; 
         }
         endT = MPI_Wtime(); 
-        /*
-        for ( auto it : ans )
-            printf("%Lf, ", it);
-        */
        
         for ( int i = 0; i <= 50 ; i += 5 ) {
             long double temp = get_exact_sol ( i * 0.02, t_target, 1e-7 );
             printf("%Lf, %Lf \n", ans[i], temp );
-            //printf("%Lf, ", ans[i]);
         } 
         printf("time: %Lf \n", (endT - beginT) );
     }    
     
-
     delete [] U;
     delete [] U_n; 
 
     MPI_Finalize();
 
-    //delete [] ans; 
+    return 0;
 }
